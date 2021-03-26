17ef7b5238ec5814b1639b0932e2a745ad8ebc34
Léo <leonardosilvapessanha@gmail.com>
2021-03-26 17:36:03 -0300

    making requests

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+
 import Header from '../../components/header';
+import { api } from '../../services/api';
 import * as S from './styles';
 
+interface IMovie {
+  Title: string;
+  Year: string;
+  Rated: string;
+  poster: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [newMovie, setNewMovie] = useState('');
+  const [movies, setMovies] = useState<IMovie>([]);
+
+  async function handleAddMovies(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    console.log('working on');
+
+    const response = await api.get<IMovie>(
+      `/?${process.env.REACT_APP_API_KEY}&t=${newMovie}`,
+    );
+    // console.log(response);
+    const movie = response.data;
+    setMovies([...movies, movie)];
+  }
   return (
     <>
       <Header />
-
       <S.Title>dash</S.Title>
-      <S.Form>
-        <input type="text" placeholder="Coloque o nome do filme aqui" />
+      <S.Form onSubmit={handleAddMovies}>
+        <input
+          value={newMovie}
+          onChange={e => setNewMovie(e.target.value)}
+          type="text"
+          placeholder="Coloque o nome do filme aqui"
+        />
         <button type="submit">Pesquisar</button>
       </S.Form>
       <S.Movies>
-        <a href="teste">
-          <img
-            src="https://image.freepik.com/fotos-gratis/camera-de-filme-antigo_62754-36.jpg"
-            alt="máquina"
-          />
-          <div>
-            <strong>Máquina</strong>
-            <p>máquina de filmar antiga</p>
-          </div>
-        </a>
         <a href="teste">
           <img
             src="https://image.freepik.com/fotos-gratis/camera-de-filme-antigo_62754-36.jpg"
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://www.omdbapi.com/',
-  params:{
-    apiKey: process.env.REACT_API_KEY,
-  }
 });
+
+export default api.defaults.params = {
+  apiKey: process.env.REACT_APP_API_KEY,
+};
