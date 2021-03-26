364d496dc6bbc51df68f2af6a90067e0d0d93297
Léo <leonardosilvapessanha@gmail.com>
2021-03-26 13:12:46 -0300

    api as an env

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header';
 import { GlobalStyle } from './styles/global';
 
+
 const App: React.FC = () => {
   return (
     <div className="App">
